4d959e59434cfeabf7add04bda0adfee215a0e94
Govindsisodia <56786328+Govindsisodia@users.noreply.github.com>
2020-10-29 12:06:15 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function one() {
+    document.getElementById("slider").src = "images/bg2.jpg";
+}
+function two() {
+    document.getElementById("slider").src = "images/bg3.jpg";
+}
+function three() {
+    document.getElementById("slider").src = "images/bg4.jpg";
+}
+function four() {
+    document.getElementById("slider").src = "images/bg5.jpg";
+}
+function five() {
+    document.getElementById("slider").src = "images/bg6.jpg";
+}
+function seven() {
+    document.getElementById("slider").src = "images/bg8.jpg";
+}
+function eight() {
+    document.getElementById("slider").src = "images/bg1.jpg";
+}
+
+setInterval(one, 3000);
+setInterval(two, 6000);
+setInterval(three, 9000);
+setInterval(four, 12000);
+setInterval(five, 18000);
+setInterval(seven, 24000);
+setInterval(eight, 27000);
+
+
